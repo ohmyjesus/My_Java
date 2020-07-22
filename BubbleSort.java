245d2d4374c7143ec6c003f017ba00af3cdfdd9f
iHoney725 <60086881+ohmyjesus@users.noreply.github.com>
2020-07-22 08:56:31 +0800

    冒泡排序

---
+++ BubbleSort.java
@@ -0,0 +1,24 @@
+package com.test;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    //冒泡排序
+    public static void main(String[] args)
+    {
+        int []arr = new int[]{99,55,42,68,-6,0,85,14,32};
+        for (int i=0;i<arr.length-1;i++)
+        {
+            for (int j=0;j<arr.length-1-i;j++)
+            {
+                if (arr[j]>arr[j+1])
+                {
+                    int a = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = a;
+                }
+            }
+        }
+        System.out.print(Arrays.toString(arr));
+    }
+}
